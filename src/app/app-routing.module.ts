e5d4ed52bb7d43a04f8872a7ae98def4f7fcdf7e
Victor Rivas <VictorRivas0230@gmail.com>
2023-07-23 09:53:24 -0400

    Add
    - Dashboard component
    - Routes for dashboard, heros and hero details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './componets/heroes/heroes.component';
+import { DashboardComponent } from './componets/dashboard/dashboard.component';
+import { HeroDetailComponent } from './componets/hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
